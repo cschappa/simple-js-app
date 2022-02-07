6e68ded090d9309a91774d20e7a709b47e5578df
cschappa <cschappa@users.noreply.github.com>
2022-02-07 15:12:33 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+alert('Hello world');
+
+let favoriteFood = 'sushi';
+let favoriteDessert = 'molten chocolate cake';
+
+document.write('My favorite food is ' + favoriteFood + ' and my favorite dessert is ' + favoriteDessert + '.');
