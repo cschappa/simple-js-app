76e2c00d14510fd92a2db0f5361281cf44032908
cschappa <cschappa@users.noreply.github.com>
2022-02-07 22:27:50 -0500

    added pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+let pokemonList = [
+    {
+        name: 'Flareon', 
+        height: 9, 
+        types: ['fire']
+    },
+    {
+        name: 'Ivysaur', 
+        height: 10,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Dewgong', 
+        height: 17,
+        types: ['ice', 'water']
+    },
+    {
+        name: 'Heracross',
+        height: 15,
+        types: ['bug', 'fighting']
+    }
+];
