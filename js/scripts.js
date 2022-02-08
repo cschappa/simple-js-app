abbacaecf64cca9a63a663f4841fb113fab2664b
cschappa <cschappa@users.noreply.github.com>
2022-02-07 22:15:20 -0500

    start of pokemonList

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-alert('Hello world');
-
-let favoriteFood = 'sushi';
-let favoriteDessert = 'molten chocolate cake';
-
-document.write('My favorite food is ' + favoriteFood + ' and my favorite dessert is ' + favoriteDessert + '.');
