93d459ca2dac4474ef01c446059de8700d294751
cschappa <cschappa@users.noreply.github.com>
2022-02-08 14:28:30 -0500

    1.3 Task

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
 let pokemonList = [
     {
-        name: 'Flareon', 
-        height: 9, 
+        name: 'Flareon',
+        height: 0.9,
         types: ['fire']
     },
     {
-        name: 'Ivysaur', 
-        height: 10,
+        name: 'Ivysaur',
+        height: 1,
         types: ['grass', 'poison']
     },
     {
-        name: 'Dewgong', 
-        height: 17,
+        name: 'Dewgong',
+        height: 1.7,
         types: ['ice', 'water']
     },
     {
         name: 'Heracross',
-        height: 15,
+        height: 1.5,
         types: ['bug', 'fighting']
     }
 ];
+
+
+// iterate throug the list of pokemon
+//   print there name and height
+//   look for big ones (> 1.5 m)
+
+for (let i = 0; i < pokemonList.length; i++) {
+
+    // check for pokemons above 1.5 meters
+    if (pokemonList[i].height > 1.5) {
+        // Over 1.5 m... Wow, these guys are big!
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow! That\'s big</p>');
+    } else {
+        // 1.5 m or less
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>');
+    }
+}
