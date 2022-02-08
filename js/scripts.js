fefba7160bc42a306b2718666e732ae4e740a61c
cschappa <cschappa@users.noreply.github.com>
2022-02-08 18:04:31 -0500

    refactored the code, fixed bug in layout

--- js/scripts.js
+++ js/scripts.js
@@ -26,14 +26,16 @@ let pokemonList = [
 //   print there name and height
 //   look for big ones (> 1.5 m)
 
+const myStuff = document.getElementById("myStuff");
+
 for (let i = 0; i < pokemonList.length; i++) {
+
+    // openning <p> tag w/ name and height
+    myStuff.innerHTML += `<p> ${pokemonList[i].name} height: ${pokemonList[i].height}`;
 
     // check for pokemons above 1.5 meters
     if (pokemonList[i].height > 1.5) {
-        // Over 1.5 m... Wow, these guys are big!
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow! That\'s big</p>');
-    } else {
-        // 1.5 m or less
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>');
+        // Over size... add some text to the end of the last element (before the closing tag)
+        myStuff.lastChild.insertAdjacentText("beforeend", ' - Wow! That\'s big!');
     }
 }
